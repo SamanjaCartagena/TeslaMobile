2b52ed6f02067f99023752dbabfcc1e27def18df
SamanjaCartagena <cart.samanja09@gmail.com>
2021-11-17 21:28:12 -0500

    background image created

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import CarItem from './assets/components/CarItem';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      
+      <CarItem/>
+    
+          <StatusBar style="auto" />
     </View>
   );
 }
@@ -18,4 +20,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  
+  
+  
 });
