40c92d6d050e16eda292819b3655b1d208748642
SamanjaCartagena <cart.samanja09@gmail.com>
2021-11-18 15:24:09 -0500

    custom and inventory button added

--- assets/components/CarItem/index.js
+++ assets/components/CarItem/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {View, Text, ImageBackground} from 'react-native';
+import StyledButton from '../StyledButton';
 import styles from './styles';
 
 const CarItem = () =>{
@@ -14,6 +15,21 @@ const CarItem = () =>{
          <Text style={styles.title}>Model S</Text>
          <Text style={styles.subtitle}>Starting at $69,420</Text>
       </View>
+
+     <StyledButton 
+        type="primary" 
+        content={"Custom Order"} 
+        onPress={() => {
+            console.warn("Custom Order was pressed")
+        }}
+         />
+          <StyledButton 
+        type="secondary" 
+        content={"Existing Inventory"} 
+        onPress={() => {
+            console.warn("Existing Inventory was pressed")
+        }}
+         />
 
       </View>
      
