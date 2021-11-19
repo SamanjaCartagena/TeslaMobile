e979d59955b6fd7a68feb713d98ee614d046f681
SamanjaCartagena <cart.samanja09@gmail.com>
2021-11-18 21:19:40 -0500

    cars list created

--- App.js
+++ App.js
@@ -1,20 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, ImageBackground, requireNativeComponent } from 'react-native';
-import CarItem from './assets/components/CarItem';
+import { StyleSheet, View } from 'react-native';
+import CarsList from './assets/components/CarsList';
+
 export default function App() {
   return (
     <View style={styles.container}>
-      
-      <CarItem 
-        name={"Model X"} 
-        tagline={"Starting from $69,00"}
-        taglineCTA={"Touchless Delivery"}
-        image={require('./assets/images/ModelX.jpeg')}
-
-        />
-    
-          <StatusBar style="auto"  />
+       <CarsList />
+       <StatusBar style="auto"  />
     </View>
   );
 }
---
+++ assets/components/CarsList/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import styles from './styles';
+import cars from './cars';
+
+
+const CarsList = (props) => {
+    return(
+        <View>
+            <Text>Not just a component</Text>
+        </View>
+    )
+}
+export default CarsList;
